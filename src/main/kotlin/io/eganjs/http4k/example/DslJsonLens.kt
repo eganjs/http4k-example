aa64c17a2f2d96b93dd0f757692daa10b2b9e1ec
Joseph Egan <joseph.egan@ukho.gov.uk>
2019-04-26 15:33:25 +0100

    Apply general refactor
    * Replace detekt with spotless as the ktlint provider plugin
    * Add format check for gradle files
    * Down version source compatibility to 8 from 11 due to incompatibilities with kapt
    * Add baseline for detekt (experimental)
    * Add gradle wrapper for version 5.4
    * Refactor code layout in DslJsonLens

--- src/main/kotlin/io/eganjs/http4k/example/DslJsonLens.kt
+++ src/main/kotlin/io/eganjs/http4k/example/DslJsonLens.kt
@@ -3,38 +3,34 @@ package io.eganjs.http4k.example
 import com.dslplatform.json.DslJson
 import com.dslplatform.json.runtime.Settings
 import org.http4k.core.ContentType
+import org.http4k.core.HttpMessage
 import org.http4k.lens.BiDiBodyLens
 import java.io.ByteArrayOutputStream
-import java.nio.charset.StandardCharsets
 
 object DslJsonLens {
+
     private const val initialBufferSize = 1024
 
     private val dslJson = DslJson<Any>(Settings.withRuntime<Any>().includeServiceLoader())
 
-    inline fun <reified T> of(): BiDiBodyLens<T> = of(T::class.java)
+    inline fun <reified T> of(): BiDiBodyLens<T> =
+        of(T::class.java)
 
-    fun <T> of(clazz: Class<T>): BiDiBodyLens<T> {
-        val threadLocalBuffer: ThreadLocal<ByteArrayOutputStream> = ThreadLocal.withInitial {
-            ByteArrayOutputStream(initialBufferSize)
-        }
-
-        return BiDiBodyLens(
+    fun <T> of(clazz: Class<T>): BiDiBodyLens<T> =
+        BiDiBodyLens(
             emptyList(),
             ContentType.APPLICATION_JSON,
-            { httpMessage ->
-                dslJson.deserialize(clazz, httpMessage.body.stream)
-            },
-            { entity, httpMessage ->
-                val buffer = threadLocalBuffer.get()
-                httpMessage.body(
-                    buffer.run {
-                        reset()
-                        dslJson.serialize(entity, buffer)
-                        toString(StandardCharsets.UTF_8)
-                    }
-                )
-            }
+            deserialiser(clazz),
+            serialiser()
         )
+
+    private fun <T> deserialiser(clazz: Class<T>): (HttpMessage) -> T = { httpMessage ->
+        dslJson.deserialize(clazz, httpMessage.body.stream)!!
+    }
+
+    private fun <T> serialiser(): (T, HttpMessage) -> HttpMessage = { entity, httpMessage ->
+        val buffer = ByteArrayOutputStream(initialBufferSize)
+        dslJson.serialize(entity, buffer)
+        httpMessage.body(buffer.toString())
     }
 }
